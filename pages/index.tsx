161675bacb62fcc69cdb30b14371e6ade4a9f63b
Bruno Alencar <joaobrunoalencar@gmail.com>
2020-04-21 18:42:36 -0300

    Added terminal layout and roboto font family by css import from google fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,31 @@
-import Link from 'next/link'
+// import Link from 'next/link'
 import Layout from '../components/layout/Layout'
 import {User} from '../interfaces/index'
 const user: User = {
   name: 'João Bruno Alencar'
 }
 
+
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+    <h1>Welcome👋</h1>
     <h2>Here i am, trying to learn Next in practice {user.name}</h2>
-    <p>
+
+    <div id="terminal">
+      <div id="terminal-current-tex"></div>
+      <div id="current-path">~</div>
+      <div id="terminal-text">$
+        <input id="terminal-input" />
+      </div>
+    </div>
+    {/* <p>
       <Link href="/about">
         <a>About</a>
       </Link>
     </p>
       <Link href="/game">
         <a>game</a>
-      </Link>
+      </Link> */}
   </Layout>
 )
 
